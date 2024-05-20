67c805641a687e441c6ccc70bd61716db4fd29b3
nariman079 <nariman079i@mail.ru>
2024-05-20 16:33:14 +0300

    fix outputdir

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir:"/home/www/",
   devServer: {
     allowedHosts: [
       'localhost',
