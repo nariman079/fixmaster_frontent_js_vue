e041849e96e71e2a35997c437e203b3fe8c635d2
nariman079 <nariman079i@mail.ru>
2024-05-22 13:33:24 +0300

    change webhistory router

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import MainPage from '@/pages/MainPage.vue'
 import DetailPage from './pages/DetailPage.vue'
@@ -9,7 +9,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
